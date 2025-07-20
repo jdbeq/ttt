af9a7f255bac4f5c779fae1f713d9e0bf6e61cea
jdbeq <jimbzy.mail@gmail.com>
2025-07-20 15:48:22 -0500

    Added global singleton to manage changing scenes.

--- play_field.gd
+++ play_field.gd
@@ -21,40 +21,49 @@ var board = [0,0,0,
 var move_count = 0
 
 # 1 for Player 1 (X) and -1 for Player 2 (O)
-var turn = 1
+var turn = -1
 
+# Checks to see if game isn't a draw.
+# Places piece
 func place_piece(pos, place) -> void:
-	# Check if game has not ended in a draw.
-	if move_count == 8:
-		print("DRAW")
-		game_over.emit()
-
 	print(active_player[turn], "'s Move...")
 	
 	# Check if square is available.
 	if board[place-1] != 0:
 		print("Invalid Move")
+		return
 	else:
 		# Creates instance of piece and places it in center of square.
-		# Marks position in board array with appropriate state.
-		if turn < 0:
+		# Marks position in board array.  Checks to see if move resulted
+		# in victory or draw.
+		if turn > 0:
 			var new_piece = Piece_X.instantiate()
 			new_piece.position = pos
 			add_child(new_piece)
 			board[place -1 ] = turn
+			check_for_draw()
+			check_for_win()
 		else:
 			var new_piece = Piece_O.instantiate()
 			new_piece.position = pos
 			add_child(new_piece)
-			board[place -1 ] = turn	
+			board[place -1 ] = turn
+			check_for_draw()
+			check_for_win()
 
-	check_for_win()
 	move_count = move_count + 1
-
+	
 	print("Move Count: ", move_count)
 	# Switch active player
 	turn *= -1
 
+func check_for_draw() -> void:
+	# Check if game has ended in a draw.
+	if move_count == 8:
+		$GameOverScreen/Label2.text = "It's a Draw!"
+		$GameOverScreen.show()
+		
+		
 func check_for_win() -> void:
 	for i in 8:
 		for j in 8:
@@ -62,55 +71,46 @@ func check_for_win() -> void:
 				if i != j and j != k and k != i:
 					if board[i] == turn and board[j] == turn and board[k] == turn:
 						if magic_grid[i] + magic_grid[j] + magic_grid[k] == 15:
-							print(turn, " Wins!")
-							game_over.emit()
+							$GameOverScreen/Label2.text = active_player[turn] + " Wins!"
+							$GameOverScreen.show()
 							return
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 func _on_square_1_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed):
-		print("1")
 		place_piece($Square_1.position, 1)
 
 func _on_square_2_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed):
-		print('2')
 		place_piece($Square_2.position,2)
 
 func _on_square_3_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed):
-		print('3')
 		place_piece($Square_3.position,3)
 
 func _on_square_4_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed):
-		print('4')
 		place_piece($Square_4.position,4)
 
 func _on_square_5_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed):
-		print('5')
 		place_piece($Square_5.position,5)
 
 func _on_square_6_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed):
-		print('6')
 		place_piece($Square_6.position,6)
 
 func _on_square_7_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed):
-		print('7')
 		place_piece($Square_7.position,7)
 
 func _on_square_8_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed):
-		print('8')
 		place_piece($Square_8.position,8)
 
 func _on_square_9_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton && event.pressed):
-		print('9')
 		place_piece($Square_9.position,9)
